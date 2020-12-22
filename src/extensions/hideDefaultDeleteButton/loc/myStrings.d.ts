43167cb6fe824f68559c4f5a587397cb3ca0cf26
Siddhartha Roy <sidtechster@gmail.com>
2020-12-23 00:00:28 +0530

    First commit

---
+++ src/extensions/hideDefaultDeleteButton/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IHideDefaultDeleteButtonApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'HideDefaultDeleteButtonApplicationCustomizerStrings' {
+  const strings: IHideDefaultDeleteButtonApplicationCustomizerStrings;
+  export = strings;
+}
